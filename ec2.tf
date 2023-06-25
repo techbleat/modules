3f00a31c1b293b7c19ebb01aabf7e4d5e56e0c62
shegoj <shegoj@yahoo.com>
2023-06-25 20:28:22 +0100

    updated module to allow ec2 name tag

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "a_machine" {
     ami = "ami-01dd271720c1ba44f"
     instance_type = "t2.micro"
     key_name = "techbleat_2023"
+
+    tags  = {
+        Name = "You cannot change me. Ha ha!"
+    }
 }
