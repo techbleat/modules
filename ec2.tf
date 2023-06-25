65d3e5c569e92dd27a6fe407e3348873b2b85d1c
shegoj <shegoj@yahoo.com>
2023-06-25 21:18:18 +0100

    count added

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "a_machine" {
     ami = "ami-01dd271720c1ba44f"
     instance_type = "t2.micro"
     key_name = var.machine_key
+    count = var.machine_count
 
     tags  = {
         Name = var.machine_name
@@ -14,3 +15,4 @@ variable machine_name {
 variable machine_key {
     default = "techbleat_2023"
 }
+variable machine_count{}
